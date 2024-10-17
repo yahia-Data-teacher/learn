d407affb8e7449066463f22bdba3f6867231444e
yahia kplr <yahiakplr@gmail.com>
2024-10-17 16:58:59 +0000

    t

--- HelloSpark/HelloSpark.py
+++ HelloSpark/HelloSpark.py
@@ -1,5 +1,7 @@
 from pyspark.sql import *
+from pyspark import SparkConf
 from lib.logger import Log4j
+from lib.utils import *
 import os
 # Define the log file directory and name
 log_directory = "/workspace/learn/app-log"  # Change to your desired path
@@ -11,16 +13,17 @@ os.makedirs(log_directory, exist_ok=True)
 if __name__ == "__main__":
     # print("Starting Hello Spark.")
     #spark.stop()
-    print(os.getcwd())
+    conf = get_spark_app_config()
+    # print(os.getcwd())
     spark =SparkSession.builder \
-        .appName("Hello Spark") \
-        .master("local[3]") \
-        .config("spark.executor.extraJavaOptions", "-Dlog4j.configuration=file:/workspace/learn/HelloSpark/log4j.properties") \
-        .config("spark.driver.extraJavaOptions", "-Dlog4j.configuration=file:/workspace/learn/HelloSpark/log4j.properties") \
+        .config(conf=conf) \
         .getOrCreate()
+        # .appName("Hello Spark").master("local[3]")
+        # .config("spark.executor.extraJavaOptions", "-Dlog4j.configuration=file:/workspace/learn/HelloSpark/log4j.properties") \
+        # .config("spark.driver.extraJavaOptions", "-Dlog4j.configuration=file:/workspace/learn/HelloSpark/log4j.properties") \
     
     # Change le niveau de log après l'initialisation
-    spark.sparkContext.setLogLevel("WARN")
+    spark.sparkContext.setLogLevel("INFO")
     logger = Log4j(spark)  # Initialise le logger
     
     
@@ -28,25 +31,25 @@ if __name__ == "__main__":
     # Log pour vérifier l'initialisation
     # logger.info("Log4j initialized successfully")
     
-    logger.info("Starting application...")
+    # logger.info("Starting application...")
 
 
-    # try:
+    try:
 
-    #     logger.info("Starting HelloSpark")
-    #     logger.info("Before starting HelloSpark")
-    #     logger.info("Starting HelloSpark")
-    #     logger.info("After starting HelloSpark")
+        logger.info("Starting HelloSpark")
+        logger.info("After starting HelloSpark")
+        conf_out = spark.sparkContext.getConf()
 
-    #     # Your processing code here
-    # except Exception as e:
-    #     logger.error(f"An error occurred: {e}")
-    #     print(f"An error occurred: {e}")
+        # Your processing code here
+    except Exception as e:
+        logger.error(f"An error occurred: {e}")
+        print(f"An error occurred: {e}")
 
     # Your processing code
 
 
 
+    logger.info(conf_out.toDebugString())
     logger.info("Application completed successfully")
     
     spark.stop()
